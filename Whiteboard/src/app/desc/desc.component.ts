000741ea2b354cd7c93f4267614ff821f60e38ad
Ilya Sekachev <v-ilseka@microsoft.com>
2019-02-18 12:52:28 +0300

    fix dwawing

--- Whiteboard/src/app/desc/desc.component.ts
+++ Whiteboard/src/app/desc/desc.component.ts
@@ -16,8 +16,8 @@ export class DescComponent {
   @Input() panel: PanelComponent;
 
 
-  private _color = () =>  this.panel.color;
-  private _width = () =>  this.panel.lineWidth;
+  private _color = () => this.panel.color;
+  private _width = () => this.panel.lineWidth;
   // constructor() {
   //   _panel = panel;
   // }
@@ -48,13 +48,23 @@ export class DescComponent {
         this.prevY = event.layerY;
       } else {
 
+        const color = this._color();
+        const width = this._width();
+        // draw line.
         this._canvasContext.beginPath();
         this._canvasContext.moveTo(this.prevX, this.prevY);
         this._canvasContext.lineTo(event.layerX, event.layerY);
-        this._canvasContext.strokeStyle = this._color();
-        this._canvasContext.lineWidth = this._width();
-        this._canvasContext.closePath();
+        this._canvasContext.strokeStyle = color;
+        this._canvasContext.lineWidth = width;
         this._canvasContext.stroke();
+        this._canvasContext.closePath();
+        // draw circle.
+        this._canvasContext.beginPath();
+        this._canvasContext.fillStyle = color;
+        this._canvasContext.arc(event.layerX, event.layerY, width / 2, 0, 2 * Math.PI);
+        this._canvasContext.fill();
+        this._canvasContext.closePath();
+
         this.prevX = event.layerX;
         this.prevY = event.layerY;
       }
